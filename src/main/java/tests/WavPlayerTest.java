c684a6091302f544b2e20ca01583ab75b5ed22b7
paulcos11 <paulcostello11@gmail.com>
2016-03-10 15:35:12 +0000

    updated pom added maven compiler olugin

--- src/main/java/tests/WavPlayerTest.java
+++ src/main/java/tests/WavPlayerTest.java
@@ -16,16 +16,15 @@ public class WavPlayerTest {
 
     @Test
     public void testNext() throws Exception {
-        Wav wav1 = new Wav("test1","test1","test1","test1");
-        Wav wav2 = new Wav("test2","test2","test2","test2");
-        List<Wav>wavs = new ArrayList<>();
+        Wav wav1 = new Wav("test1", "test1", "test1", "test1");
+        Wav wav2 = new Wav("test2", "test2", "test2", "test2");
+        List<Wav> wavs = new ArrayList<>();
         wavs.add(wav1);
         wavs.add(wav2);
         WavPlayer wavPlayer = new WavPlayer(wavs);
         wavPlayer.setWav(1);
-        assertEquals("test2",wavs.get(1).getTrackName());
+        assertEquals("test2", wavs.get(1).getTrackName());
         wavPlayer.setWav(0);
-        assertEquals("test1",wavs.get(0).getTrackName());
+        assertEquals("test1", wavs.get(0).getTrackName());
     }
-
 }
