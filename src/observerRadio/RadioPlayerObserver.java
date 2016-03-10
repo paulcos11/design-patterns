c075bc80c83c7a7ea70eb5757f1cc0df8131c4dd
paulcos11 <paulcostello11@gmail.com>
2016-03-10 00:54:22 +0000

    updated project to be maven project

--- src/observerRadio/RadioPlayerObserver.java
+++
@@ -1,9 +0,0 @@
-package observerRadio;
-
-/**
- *
- * Created by Paul turnOnRadio 23/11/2015.
- */
-public interface RadioPlayerObserver {
-    void update(String name, String description, String origin, String links);
-}
